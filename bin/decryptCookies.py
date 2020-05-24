09f175003f0060b652d21f3cbfd6de8a9a9ec2c9
Kevin Cui <krazycavin@gmail.com>
2020-05-24 10:43:36 +0200

    Add python script to decrypt chromium cookies

---
+++ bin/decryptCookies.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python3
+import sys
+from sqlite3 import dbapi2
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Cipher import AES
+
+
+def main(db):
+    conn = dbapi2.connect(db)
+    query = 'SELECT name,encrypted_value FROM cookies \
+            WHERE host_key="getpocket.com"'
+    rows = conn.cursor().execute(query)
+
+    for r in rows.fetchall():
+        print(r[0] + ':' + decrypt(r[1]) + ';')
+
+
+def clean(x):
+    return x[:-x[-1]].decode('utf8')
+
+
+def decrypt(blob):
+    # Code modified from https://stackoverflow.com/a/23727331/8272771
+    encrypted_value = blob[3:]
+    salt = b'saltysalt'
+    iv = b' ' * 16
+    length = 16
+    password = 'peanuts'.encode('utf8')
+    iterations = 1
+
+    key = PBKDF2(password, salt, length, iterations)
+    cipher = AES.new(key, AES.MODE_CBC, IV=iv)
+
+    decrypted = cipher.decrypt(encrypted_value)
+
+    return clean(decrypted)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
